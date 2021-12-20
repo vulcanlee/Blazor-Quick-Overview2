7572a6ef844cf52c5505f705ca999d8bf6109285
Vulcan Lee <vulcan.lee@gmail.com>
2021-12-20 15:37:11 +0800

    建立部落格文章頁面之 CRUD 功能

---
+++ NET6/bh03/Blogger/Program.cs
@@ -0,0 +1,41 @@
+using Blogger.Data;
+using Blogger.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+#region 宣告 BlogDbContext 會用到的服務
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BlogDbContext>(
+    options =>
+    options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();

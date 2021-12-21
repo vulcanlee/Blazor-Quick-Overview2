c330389af16c2060b10d0601be728ba7d83ed598
Vulcan Lee <vulcan.lee@gmail.com>
2021-12-21 11:31:14 +0800

    將資料庫存取程式碼分離出來

---
+++ NET6/bh07/Blogger/Models/BlogDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blogger.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> context) : base(context)
+        {
+        }
+        public DbSet<BlogPost> BlogPost { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Chinese_Taiwan_Stroke_CI_AS");
+
+            #region 設定階層級的刪除政策(預設若關聯子資料表有紀錄，父資料表不可強制刪除
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            #endregion
+        }
+    }
+}
